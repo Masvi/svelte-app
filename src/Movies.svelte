aca205e6cfd7b099cf1d5f389edb9bdd4cf2c2b9
masvi <jeanmasvi@gmail.com>
2022-10-09 18:34:35 -0300

    chore: setup env

---
+++ src/Movies.svelte
@@ -0,0 +1,10 @@
+<script>
+  console.log(process.env('PRIVATE_KEY'));
+  async function getMovies() {
+    const res = await fetch(`https://the-one-api.dev/v2`, {
+      headers: {
+        Authorization: `Bearer ${PRIVATE_KEY}`,
+      },
+    });
+  }
+</script>
